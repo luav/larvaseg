4b8cc1b8d5e09ae782222edc5a80657780d35362
Artem <luardev@gmail.com>
2020-10-23 16:31:19 +0200

    Output dir fixed

--- larvaseg.py
+++ larvaseg.py
@@ -62,15 +62,22 @@ if __name__ == '__main__':
 			fps = cap.get(cv.CAP_PROP_FPS)
 			width = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
 			height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
-			print('fps: {}, size: {}x{}'.format(fps, width, height))
+			#ofname = oa.path.split(ifname)[1]
+			ofname = os.path.join(args.outp_dir, 'v4_v5_f.05_clahe32.avi')
+			print('	fps: {}, size: {}x{}\n\toutput: {}'.format(fps, width, height, ofname))
 			# Define the codec and create VideoWriter object.The output is stored in 'outpy.avi' file.
 			# Define the fps to be equal to 10. Also frame size is passed.
 			# FMP4, MJPG, XVID
 			#fourcc = cv.VideoWriter_fourcc(*'MJPG')
-			out = cv.VideoWriter('outp-f.8.avi', cv.VideoWriter_fourcc(*'XVID'), fps, (width, height))
+			out = cv.VideoWriter(ofname, cv.VideoWriter_fourcc(*'XVID'), fps, (width, height))
 			if not cap:
 				exit(1)
 			title = os.path.split(ifname)[1]
+			#cv.namedWindow(title, cv.WINDOW_NORMAL)
+			#wWnd = 1600
+			#rfont = width / wWnd
+			#cv.resizeWindow(title, wWnd, int(height / rfont))
+			#rfont = max(1, rfont)
 			nframe = 0
 			while cap.isOpened():
 				# Capture frame-by-frame
@@ -78,32 +85,44 @@ if __name__ == '__main__':
 				if not ret:
 					assert frame is None
 					break
-				cv.imshow(title + '#' + str(nframe), frame)
+				nframe += 1
+				#_, _, w, _ = cv.getWindowImageRect(title)
+				#rfont = max(1, width / w)
+				#cv.putText(frame,'Recording ...',
+				#	(20, 20 + int(24 * rfont)),  # bottomLeftCornerOfText
+				#	cv.FONT_HERSHEY_SIMPLEX, 
+				#	rfont,  # Font size
+				#	(7, 7, 255),  # Color
+				#	1 + round(rfont)) # Line thickness, px
+				cv.imshow(title, frame)
 				# Convert to grayscale
 				gray = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
+				# Apply CLAHE (Contrast Limited Adaptive Histogram Equalization)
+				clahe = cv.createCLAHE(clipLimit=32.0, tileGridSize=(8,8))  # clipLimit=40 / 255  Threshold for contrast limiting.
+				gray = clahe.apply(gray)
 				# Adaprive optimal thresholds: THRESH_OTSU, THRESH_TRIANGLE
 				ret, thresh = cv.threshold(gray, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)  # THRESH_TRIANGLE
-				cv.imshow(title + '_thresh#' + str(nframe), thresh)
-				# cv.imshow(title + '_gray#' + str(nframe), frame)
+				cv.imshow(title + '_thresh', thresh)
+				# cv.imshow(title + '_gray', frame)
 
 				# Noise removal
 				kernel = np.ones((3, 3), np.uint8)  # 3, 3 
 				opening = cv.morphologyEx(thresh, cv.MORPH_OPEN, kernel, iterations=2) # orig: 2
 				# opening = cv.dilate(thresh, kernel, iterations=2) # 3
-				cv.imshow(title + '_opening#' + str(nframe), opening)
+				cv.imshow(title + '_opening', opening)
 				# opening = thresh
 				# # Background identification
 				sure_bg = cv.dilate(opening, kernel, iterations=3) # orig: 3
-				#sure_bg = cv.morphologyEx(opening, cv.MORPH_OPEN, kernel, iterations=2) # 2
-				cv.imshow(title + '_bg#' + str(nframe), sure_bg)
-				# sure_bg = opening
+				# #sure_bg = cv.morphologyEx(opening, cv.MORPH_OPEN, kernel, iterations=2) # 2
+				# cv.imshow(title + '_bg', sure_bg)
+				# # sure_bg = opening
 				# Foreground identification
 				dist_transform = cv.distanceTransform(opening, cv.DIST_L2, 5)  # 5 or 3
-				ret, sure_fg = cv.threshold(dist_transform, 0.8 * dist_transform.max(), 255, 0)  # 0.7 [0.5 - deliv]]
+				ret, sure_fg = cv.threshold(dist_transform, 0.05 * dist_transform.max(), 255, 0)  # 0.7 [0.5 - deliv]]
 				# Remaining areas
 				sure_fg = np.uint8(sure_fg)
 				unknown = cv.subtract(sure_bg, sure_fg)
-				cv.imshow(title + '_unknown#' + str(nframe), unknown)
+				# cv.imshow(title + '_unknown', unknown)
 				
 				# Marker labelling
 				ret, markers = cv.connectedComponents(sure_fg)
@@ -114,7 +133,7 @@ if __name__ == '__main__':
 				# Apply watershed
 				markers = cv.watershed(frame, markers)
 				frame[markers == -1] = [255,0,0]
-				cv.imshow(title + '_gray#' + str(nframe), frame)
+				cv.imshow(title + '_gray', frame)
 				
 				# Save output video
 				out.write(frame)
